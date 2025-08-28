0fdd2ce15f13c3a85b98da5eadc4ea2b29e10284
Ayush Kumar <ayushkumarkvs160@gmail.com>
2025-08-28 09:55:59 +0530

    Time: 12 ms (80.81%) | Memory: 23.6 MB (59.83%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,30 +1,48 @@
+# class KthLargest:
+
+#     def __init__(self, k: int, nums: List[int]):
+#         self.k = k
+#         self.stream = nums
+#         self.stream.sort()
+
+#     def add(self, val: int) -> int:
+#         index = self.getIndex(val)
+#         self.stream.insert(index,val)
+#         return self.stream[-self.k]        
+    
+#     def getIndex(self, val: int) -> int:
+#         left,right = 0,len(self.stream)-1
+#         while left <= right:
+#             mid = (left+right)//2
+#             mid_element = self.stream[mid]
+#             if mid_element == val:
+#                 return mid 
+#             elif mid_element > val:
+#                 right = mid - 1
+#             else:
+#                 left = mid + 1
+
+#         return left
+
+# # Your KthLargest object will be instantiated and called as such:
+# # obj = KthLargest(k, nums)
+# # param_1 = obj.add(val)
+
+
+import heapq
+from typing import List
+
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.stream = nums
-        self.stream.sort()
+        self.minHeap = nums
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
 
     def add(self, val: int) -> int:
-        index = self.getIndex(val)
-        self.stream.insert(index,val)
-        return self.stream[-self.k]        
-    
-    def getIndex(self, val: int) -> int:
-        left,right = 0,len(self.stream)-1
-        while left <= right:
-            mid = (left+right)//2
-            mid_element = self.stream[mid]
-            if mid_element == val:
-                return mid 
-            elif mid_element > val:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        return left
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
-
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
